b76cc660c6210b44525328542d29f1815acf393a
legendary0911 <85240150+legendary0911@users.noreply.github.com>
2023-06-22 22:36:55 +0530

    few changes

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -1,15 +1,15 @@
 import jwt from 'jsonwebtoken';
 
-export const verifyToken = (req,res,next) => {
+export const verifyToken = (req, res, next) => {
     const token = req.cookies.accessToken
 
-    if(!token){
-        return res.status(401).json({ success: false, message: "You're not authorize"})
+    if (!token) {
+        return res.status(401).json({ success: false, message: "You're not authorize no token" })
     }
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err,user)=>{
-        if(err){
-            return res.status(401).json({ success: false, message: "token is invalid"})
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(401).json({ success: false, message: "token is invalid" })
         }
 
         req.user = user
@@ -18,22 +18,22 @@ export const verifyToken = (req,res,next) => {
 }
 
 
-export const verifyUser = (req,res,next)=> {
-    verifyToken(req,res,next,()=>{
-        if(req.user.id === req.params.id || req.user.role==='admin'){
+export const verifyUser = (req, res, next) => {
+    verifyToken(req, res, next, () => {
+        if (req.user.id === req.params.id || req.user.role === 'admin') {
             next()
-        }else{
-            return res.status(401).json({ success: false, message: "You're not authenticated"})
+        } else {
+            return res.status(401).json({ success: false, message: "You're not authenticated" })
         }
     })
 }
 
-export const verifyAdmin = (req,res,next)=> {
-    verifyToken(req,res,next,()=>{
-        if(req.user.role==='admin'){
+export const verifyAdmin = (req, res, next) => {
+    verifyToken(req, res, next, () => {
+        if (req.user.role === 'admin') {
             next()
-        }else{
-            return res.status(401).json({ success: false, message: "You're not authorize"})
+        } else {
+            return res.status(401).json({ success: false, message: "You're not authorize not admin" })
         }
     })
 }
